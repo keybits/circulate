2ac32971aa07f648f32246b9696e9266e31c0feb
Tom Atkins <tom@keybits.net>
2017-03-15 14:56:31 +0000

    Set up Heroku on CCI11

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
 tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
