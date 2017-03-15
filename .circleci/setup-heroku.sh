48a46231d8be26209bf5067562f7f583476ebd87
Tom Atkins <tom@keybits.net>
2017-03-15 14:47:35 +0000

    Set up Heroku on CCI7

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,6 +1,6 @@
-wget https://cli-assets.heroku.com/branches/stable/heroku-linux-64.tar.gz
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
-tar -xvzf heroku-linux-64.tar.gz -C /usr/local/lib
+tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
 ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
