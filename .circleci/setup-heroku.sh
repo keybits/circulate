418965fb6d4699fd78e2026d994f7ac2324ef381
Tom Atkins <tom@keybits.net>
2017-03-15 15:25:47 +0000

    Try a Heroku deploy7

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git remote add heroku https://git.heroku.com/cci-demo-walkthrough.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
 tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
